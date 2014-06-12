04f05135e318ea5629c20dfd266007f607098ce4
daixi <xi.dai1990@gmail.com>
2014-06-12 15:53:33 +0800

    merge sort

--- src/xidai/xsort/xsort.py
+++ src/xidai/xsort/xsort.py
@@ -1,10 +1,12 @@
 __author__ = 'Xidai'
+import sys
 
 
 def insertion_sort(a):
     """
     For A[a1, a2, a3..., an], a1 <= a2 <= a3... <= an
     Sorted in place.
+    O(n**2)
     """
     for i in range(1, len(a)):
         key = a[i]
@@ -29,3 +31,46 @@ def insertion_sort_descend(a):
         a[j + 1] = key
 
     return a
+
+
+def merge_sort(a, p, r):
+    """
+    :param a: List needed to be sorted.
+    :param p: Starting index.
+    :param r: Ending index
+    :return: Sorted list
+    """
+
+    if p < r:
+        q = (p + r) / 2
+        merge_sort(a, p, q)
+        merge_sort(a, q + 1, r)
+        merge(a, p, q, r)
+
+    return a
+
+
+def merge(a, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    left = []
+    right = []
+    for i in range(0, n1):
+        left.append(a[p + i])
+    for j in range(0, n2):
+        right.append(a[q + 1 + j])
+    right.append(sys.maxint)
+    left.append(sys.maxint)
+
+    i, j = 0, 0
+    for k in range(p, r + 1):
+        if left[i] <= right[j]:
+            a[k] = left[i]
+            i += 1
+        else:
+            a[k] = right[j]
+            j += 1
+
+    return a
+
+
--- test/xidai/sort_test.py
+++ test/xidai/sort_test.py
@@ -31,5 +31,19 @@ class TestSort(unittest.TestCase):
         sorted_c = xsort.insertion_sort_descend(c)
         self.assertEqual(sorted_c, [3, 2, 1])
 
+    def test_merge_sort(self):
+        a = [1, 3, 4, 2]
+        sorted_a = xsort.merge_sort(a, 0, len(a) - 1)
+        self.assertEqual(sorted_a, [1, 2, 3, 4])
+
+        b = [1, 3, 2]
+        sorted_b = xsort.merge_sort(b, 0, len(b) - 1)
+        self.assertEqual(sorted_b, [1, 2, 3])
+
+        c = [1, 1, 5, 6, 2, 2, 9]
+        sorted_c = xsort.merge_sort(c, 0, len(c) - 1)
+        self.assertEqual(sorted_c, [1, 1, 2, 2, 5, 6, 9])
+
+
 if __name__ == "__main__":
     unittest.main()
