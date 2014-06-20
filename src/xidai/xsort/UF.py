902e18d65ae712e0be810a67b77d349b8f96f69f
daixi <xi.dai1990@gmail.com>
2014-06-20 14:20:51 +0800

    weighted quick union.

--- src/xidai/xsort/UF.py
+++ src/xidai/xsort/UF.py
@@ -38,3 +38,32 @@ class QuickUnionUF:
         root_p = self.root(p)
         root_q = self.root(q)
         self.id[root_p] = root_q
+
+
+class WeightedQuickUnionUF:
+    def __init__(self, n):
+        self.id = []
+        self.size = []
+        for i in range(0, n):
+            self.id[i] = i
+            self.size[i] = 1
+
+    def root(self, i):
+        if self.id[i] == i:
+            return i
+        else:
+            return self.root(self.id[i])
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def union(self, p, q):
+        root_p = self.root(p)
+        root_q = self.root(q)
+        if root_p != root_q:
+            if self.size[root_p] <= self.size[root_q]:
+                self.id[root_p] = root_q
+                self.size[root_q] += self.size[root_p]
+            else:
+                self.id[root_q] = root_p
+                self.size[root_p] += self.size[root_q]
